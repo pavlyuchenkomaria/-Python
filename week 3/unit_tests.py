080cb180dc03dc70f7a46cc7a0dbfc8468d1f609
Masha <pavluchenkomaria@gmail.com>
2018-08-23 17:19:27 +0700

    Даны действительные коэффициенты a, b, c, при этом a != 0. Решите квадратное уравнение ax²+bx+c=0 и выведите все его корни. Формат ввода Вводятся три действительных числа. Формат вывода Если уравнение имеет два корня, выведите два корня в порядке возрастания, если один корень — выведите одно число, если нет корней — не выводите ничего.
    решено 2 способами

--- week 3/unit_tests.py
+++ week 3/unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from math import sqrt
 
 
 def my_sum(a, b):
@@ -8,5 +9,50 @@ class SumTests(unittest.TestCase):
     def test_add(self):
         self.assertEqual(my_sum(1, 2), 3)
 
+
+def solver(a, b, c):
+    if b == 0 and c == 0:
+        return [0]
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        x1 = (-b - sqrt(d)) / (2 * a)
+        x2 = (-b + sqrt(d)) / (2 * a)
+        return [x1, x2]
+    elif d == 0:
+        x = -b / (2 * a)
+        return [x]
+
+
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    a = float(input())
+    b = float(input())
+    c = float(input())
+
+    solution = solver(a, b, c)
+    if len(solution) == 0:
+        raise SystemExit
+    elif len(solution) == 1:
+        x = solution[0]
+        if x.is_integer():
+            x = int(x)
+        else:
+            x = '{0:.6f}'.format(-b / (2 * a))
+        print(x)
+    elif len(solution) == 2:
+        x1 = solution[0]
+        x2 = solution[1]
+        if x1.is_integer():
+            x1 = int(x1)
+        else:
+            x1 = '{0:.6f}'.format(x1)
+        if x2.is_integer():
+            x2 = int(x2)
+        else:
+            x2 = '{0:.5f}'.format(x2)
+        if x1 > x2:
+            print(x2, x1)
+        elif x1 < x2:
+            print(x1, x2)
+    else:
+        raise SystemExit
