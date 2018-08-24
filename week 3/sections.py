83c2b52eb8cccace2fcdf8bfb6c8cd081c23c223
Masha <pavluchenkomaria@gmail.com>
2018-08-25 03:20:57 +0700

    base things with sections Формат ввода Дана строка. Формат вывода Сначала выведите третий символ этой строки. Во второй строке выведите предпоследний символ этой строки. В третьей строке выведите первые пять символов этой строки. В четвертой строке выведите всю строку, кроме последних двух символов. В пятой строке выведите все символы с четными индексами (считая, что индексация начинается с 0, поэтому символы выводятся начиная с первого). В шестой строке выведите все символы с нечетными индексами, то есть начиная со второго символа строки. В седьмой строке выведите все символы в обратном порядке. В восьмой строке выведите все символы строки через один в обратном порядке, начиная с последнего. В девятой строке выведите длину данной строки.

---
+++ week 3/sections.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    stroka = input()
+    print(stroka[2])
+    print(stroka[-2])
+    print(stroka[:5])
+    print(stroka[:-2])
+    print(stroka[::2])
+    print(stroka[1::2])
+    print(stroka[::-1])
+    a = stroka[::-1]
+    print(a[::2])
+    print(len(stroka))
