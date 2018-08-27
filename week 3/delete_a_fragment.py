3d60c2a073e80637b1906b4aa385666f9514a45f
Masha <pavluchenkomaria@gmail.com>
2018-08-27 15:30:07 +0700

    Дана строка, в которой буква h встречается минимум два раза.Удалите из этой строки первое и последнее вхождение буквы h,а также все символы, находящиеся между ними. Формат ввода Вводится строка. Решается 2 способами.

--- week 3/delete_a_fragment.py
+++ week 3/delete_a_fragment.py
@@ -4,3 +4,10 @@ if __name__ == '__main__':
     reverted_string = string[::-1]
     b = len(string) - reverted_string.find('h') - 1
     print(string[:a] + string[b + 1:])
+
+    output_string = ""
+    for i in range(0, a):
+        output_string += string[i]
+    for i in range(b + 1, len(string)):
+        output_string += string[i]
+    print(output_string)
